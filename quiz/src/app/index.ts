004417c3556918c2f512456ebc552e6844f0a719
rborisov <rborisov@codemastersintl.com>
2023-11-25 22:40:42 +0300

    added eslint and editorconfig

--- quiz/src/app/index.ts
+++ quiz/src/app/index.ts
@@ -1,7 +1,7 @@
-import { IQuestion } from './models/question';
-import { IAnswer, IUserSelectedAnswer } from './models/answer';
-import { calculateResult, checkAnswer } from './services/quiz-service.js';
-import { getQuestions } from './services/questions-service.js';
+import {IQuestion} from './models/question';
+import {IAnswer, IUserSelectedAnswer} from './models/answer';
+import {calculateResult, checkAnswer} from './services/quiz-service.js';
+import {getQuestions} from './services/questions-service.js';
 
 const questionEl = document.querySelector<HTMLHeadingElement>('#question-text') as HTMLHeadingElement;
 const answerBtns = document.querySelector<HTMLDivElement>('#answer-buttons') as HTMLDivElement;
@@ -14,104 +14,104 @@ let answers: IUserSelectedAnswer[] = [];
 window.addEventListener('DOMContentLoaded', init);
 
 async function init() {
-        nextBtn.addEventListener('click', async () => {
-                if (currentQuestionIdx < questions.length) {
-                        await goToNextQuestion();
-                } else {
-                        startQuiz();
-                }
-        })
+    nextBtn.addEventListener('click', async () => {
+        if (currentQuestionIdx < questions.length) {
+            await goToNextQuestion();
+        } else {
+            startQuiz();
+        }
+    });
 
-        questions = await getQuestions();
+    questions = await getQuestions();
 
-        startQuiz();
+    startQuiz();
 }
 
 function startQuiz() {
-        currentQuestionIdx = 0;
-        showQuestion();
+    currentQuestionIdx = 0;
+    showQuestion();
 }
 
 function showQuestion() {
-        resetState();
+    resetState();
 
-        let currentQuestion = questions[currentQuestionIdx];
-        let questionNumber = currentQuestionIdx + 1;
+    let currentQuestion = questions[currentQuestionIdx];
+    let questionNumber = currentQuestionIdx + 1;
 
-        questionEl.innerHTML = `${questionNumber}. ${currentQuestion.question}`;
+    questionEl.innerHTML = `${questionNumber}. ${currentQuestion.question}`;
 
-        currentQuestion.answers.forEach(createAnswerButton);
+    currentQuestion.answers.forEach(createAnswerButton);
 }
 
 function resetState() {
-        nextBtn.style.display = "none";
-        while (answerBtns.firstChild) {
-                answerBtns.removeChild(answerBtns.firstChild);
-        }
+    nextBtn.style.display = 'none';
+    while (answerBtns.firstChild) {
+        answerBtns.removeChild(answerBtns.firstChild);
+    }
 }
 
 function createAnswerButton(answer: IAnswer) {
-        const button = document.createElement('button');
-        button.innerHTML = answer.text;
-        button.type = 'button';
-        button.classList.add('button', 'question__answer');
-        answerBtns.appendChild(button);
+    const button = document.createElement('button');
+    button.innerHTML = answer.text;
+    button.type = 'button';
+    button.classList.add('button', 'question__answer');
+    answerBtns.appendChild(button);
 
-        button.dataset.id = answer.id.toString();
-        button.addEventListener('click', selectAnswer)
+    button.dataset.id = answer.id.toString();
+    button.addEventListener('click', selectAnswer);
 }
 
 async function selectAnswer(e: MouseEvent) {
-        const selectedBtn = e.target as HTMLButtonElement;
-        if (!selectedBtn.dataset.id) {
+    const selectedBtn = e.target as HTMLButtonElement;
+    if (!selectedBtn.dataset.id) {
+        throw Error('Can not find id info');
+    }
+    const answerId = +selectedBtn.dataset.id;
+    const {isCorrect, correctAnswerId} = await checkAnswer(answerId, questions[currentQuestionIdx].id);
+
+    if (isCorrect) {
+        selectedBtn.classList.add('question__answer--correct');
+    } else {
+        selectedBtn.classList.add('question__answer--incorrect');
+    }
+
+    Array.from(answerBtns.children)
+        .filter((element) => element instanceof HTMLButtonElement)
+        .forEach((b: Element) => {
+            let button: HTMLButtonElement = b as HTMLButtonElement; // If we use force typing in 80 line it does not work.
+
+            if (!button.dataset.id) {
                 throw Error('Can not find id info');
-        }
-        const answerId = +selectedBtn.dataset.id;
-        const {isCorrect, correctAnswerId} = await checkAnswer(answerId, questions[currentQuestionIdx].id);
+            }
 
-        if (isCorrect) {
-                selectedBtn.classList.add('question__answer--correct');
-        } else {
-                selectedBtn.classList.add('question__answer--incorrect');
-        }
-
-        Array.from(answerBtns.children)
-                .filter((element) => element instanceof HTMLButtonElement)
-                .forEach((b: Element) => {
-                        let button: HTMLButtonElement = b as HTMLButtonElement; // If we use force typing in 80 line it does not work.
-
-                        if (!button.dataset.id) {
-                                throw Error('Can not find id info');
-                        }
-
-                        if (+button.dataset.id === correctAnswerId) {
-                                button.classList.add('question__answer--correct');
-                        }
-                        button.disabled = true;
-                });
-
-        answers.push({
-                questionId: questions[currentQuestionIdx].id,
-                answerId
+            if (+button.dataset.id === correctAnswerId) {
+                button.classList.add('question__answer--correct');
+            }
+            button.disabled = true;
         });
-        nextBtn.style.display = 'block';
+
+    answers.push({
+        questionId: questions[currentQuestionIdx].id,
+        answerId
+    });
+    nextBtn.style.display = 'block';
 }
 
 async function goToNextQuestion() {
-        currentQuestionIdx++;
-        if (currentQuestionIdx < questions.length) {
-                showQuestion();
-        } else {
-                await showScore();
-        }
+    currentQuestionIdx++;
+    if (currentQuestionIdx < questions.length) {
+        showQuestion();
+    } else {
+        await showScore();
+    }
 }
 
 async function showScore() {
-        resetState();
-        const {score} = await calculateResult(answers);
-        answers = [];
+    resetState();
+    const {score} = await calculateResult(answers);
+    answers = [];
 
-        questionEl.innerHTML = `You scored ${score} out of ${questions.length}`;
-        nextBtn.innerHTML = 'Play Again';
-        nextBtn.style.display = 'block';
+    questionEl.innerHTML = `You scored ${score} out of ${questions.length}`;
+    nextBtn.innerHTML = 'Play Again';
+    nextBtn.style.display = 'block';
 }
--- quiz/src/app/services/rest.ts
+++ quiz/src/app/services/rest.ts
@@ -1,12 +1,12 @@
 const SERVER_URL = 'http://localhost:3000/';
 
 export async function get<T>(url: string): Promise<T> {
-    const api = `${SERVER_URL}${url}`
+    const api = `${SERVER_URL}${url}`;
 
     return (await fetch(api)).json();
 }
 
-export async function post<T>(url: string, data: any): Promise<T> {
+export async function post<T>(url: string, data: unknown): Promise<T> {
     const api = `${SERVER_URL}${url}`;
 
     return (await fetch(api, {
@@ -17,4 +17,3 @@ export async function post<T>(url: string, data: any): Promise<T> {
         body: JSON.stringify(data)
     })).json();
 }
-
