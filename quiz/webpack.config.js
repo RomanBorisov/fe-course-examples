0ba2752e3dd13a0c858d5018547e483b5d65b04d
rborisov <rborisov@codemastersintl.com>
2023-11-25 23:15:45 +0300

    fixed some npm scripts

--- quiz/webpack.config.js
+++ quiz/webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
